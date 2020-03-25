c83eaf6146196f18cf415bea99925f715024cf35
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-25 04:19:33 -0700

    comit

--- src/main/java/com/uabc/sap/controller/ProductoController.java
+++ src/main/java/com/uabc/sap/controller/ProductoController.java
@@ -3,6 +3,8 @@ package com.uabc.sap.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/p")
@@ -14,8 +16,17 @@ public class ProductoController {
         return "index_prod";
     }
 
-    @RequestMapping("/saveProducto")
-    public String saveProducto(){
+    @RequestMapping(path="/saveProducto",method = RequestMethod.POST)
+    public String saveProducto(
+    @RequestParam(value = "type", required = true) String type,
+    @RequestParam(value = "race", required = true) String race,
+    @RequestParam(value = "color", required = true) String color,
+    @RequestParam(value = "fur", required = true) String fur,
+    @RequestParam(value = "date", required = true) String date,
+    @RequestParam(value = "vaccinated", required = false, defaultValue = "false") boolean vaccinated,
+    @RequestParam(value = "adopted", required = false, defaultValue = "false") boolean adopted,
+    @RequestParam(value = "responsable", required = false) String responsable,
+    @RequestParam(value = "img", required = false) MultipartFile img){
 
         return "redirect:/p/";
     }    
