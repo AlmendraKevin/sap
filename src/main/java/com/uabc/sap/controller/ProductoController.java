1bff761172da553225c735821aad2f05b78230a1
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-25 04:47:33 -0700

    SQL FINAL

--- src/main/java/com/uabc/sap/controller/ProductoController.java
+++ src/main/java/com/uabc/sap/controller/ProductoController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/p")
@@ -18,15 +19,12 @@ public class ProductoController {
 
     @RequestMapping(path="/saveArticle",method = RequestMethod.POST)
     public String saveProducto(
-    @RequestParam(value = "id_revista", required = true) Integer id,
-    @RequestParam(value = "race", required = true) String race,
-    @RequestParam(value = "color", required = true) String color,
-    @RequestParam(value = "fur", required = true) String fur,
-    @RequestParam(value = "date", required = true) String date,
-    @RequestParam(value = "vaccinated", required = false, defaultValue = "false") boolean vaccinated,
-    @RequestParam(value = "adopted", required = false, defaultValue = "false") boolean adopted,
-    @RequestParam(value = "responsable", required = false) String responsable,
-    @RequestParam(value = "img", required = false) MultipartFile img){
+        /** parametros para entidad producto */
+    @RequestParam(value = "id_revista", required = true) Integer id_revista,
+    @RequestParam(value = "id_usuario", required = true) String id_usuario,
+    @RequestParam(value = "id_autor", required = true) String id_autor,
+    @RequestParam(value = "id_articulo", required = true) String id_articulo,
+    @RequestParam(value = "file", required = true) MultipartFile file){
 
         return "redirect:/p/";
     }    
