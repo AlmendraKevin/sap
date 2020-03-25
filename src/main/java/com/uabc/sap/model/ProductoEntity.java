fade38b1b57437d312f8f6c20c711cfd4d804ade
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-25 03:45:46 -0700

    index controller funciona

--- src/main/java/com/uabc/sap/model/ProductoEntity.java
+++ src/main/java/com/uabc/sap/model/ProductoEntity.java
@@ -1,4 +1,18 @@
 package com.uabc.sap.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="producto")
 public class ProductoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+        
+
 }
