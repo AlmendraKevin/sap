a72840baf59799d31caa0b91605bfe87a8a1ac24
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-25 04:57:34 -0700

    saveProducto controller

--- src/main/java/com/uabc/sap/controller/ProductoController.java
+++ src/main/java/com/uabc/sap/controller/ProductoController.java
@@ -1,5 +1,11 @@
 package com.uabc.sap.controller;
 
+import java.util.Base64;
+
+import com.uabc.sap.model.ProductoEntity;
+import com.uabc.sap.service.ProductoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +16,9 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 @RequestMapping("/p")
 public class ProductoController {
+
+    @Autowired
+    ProductoService service;
 
     @RequestMapping
     public String getHome(Model model){
@@ -22,10 +31,22 @@ public class ProductoController {
         /** parametros para entidad producto */
     @RequestParam(value = "id_revista", required = true) Integer id_revista,
     @RequestParam(value = "id_usuario", required = true) String id_usuario,
-    @RequestParam(value = "id_autor", required = true) String id_autor,
-    @RequestParam(value = "id_articulo", required = true) String id_articulo,
-    @RequestParam(value = "file", required = true) MultipartFile file){
+    @RequestParam(value = "file", required = true) MultipartFile file,
+    @RequestParam(value = "nombre_autor", required = true) String nombre_autor,
+    @RequestParam(value = "apellido_paterno_autor", required = true) String apellido_paterno_autor,
+    @RequestParam(value = "apellido_materno_autor", required = true) String apellido_materno_autor,
+    @RequestParam(value = "grado_estudio_autor", required = true) String grado_estudio_autor,
+    @RequestParam(value = "nombre_articulo", required = true) String nombre_articulo,
+    @RequestParam(value = "issn_articulo", required = true) String issn_articulo,
+    @RequestParam(value = "indizado", required = true) String indizado,
+    @RequestParam(value = "arbitraje", required = true) String arbitraje,
+    @RequestParam(value = "fecha_publicacion", required = true) String fecha_publicacion    
+    ){
+        ProductoEntity prod = new ProductoEntity();
+        /** setters atribs */
+        String archivo = Base64.getEncoder().encodeToString(file.getBytes());
 
+        service.save(prod);
         return "redirect:/p/";
     }    
 
