505898d54f2a99fa384860512a018817100eec83
pablo-alejos <pablo.alejos@uabc.edu.mx>
2020-03-25 04:14:56 -0700

    controladores prod

--- src/main/java/com/uabc/sap/controller/ProductoController.java
+++ src/main/java/com/uabc/sap/controller/ProductoController.java
@@ -1,4 +1,23 @@
 package com.uabc.sap.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/p")
 public class ProductoController {
+
+    @RequestMapping
+    public String getHome(Model model){
+
+        return "index_prod";
+    }
+
+    @RequestMapping("/saveProducto")
+    public String saveProducto(){
+
+        return "redirect:/p/";
+    }    
+
 }
